122543963407a618e38db965ebe61911180b6dd3
Jack Clayton <clayjs0@gmail.com>
2020-12-29 16:21:02 -0700

    Add specs for action

---
+++ spec/jahuty/action/show_spec.rb
@@ -0,0 +1,25 @@
+module Jahuty
+  module Action
+    RSpec.describe Show do
+      describe "#initialize" do
+        let(:id)       { 1 }
+        let(:resource) { "foo" }
+        let(:params)   { { bar: "baz" } }
+
+        subject(:show) { Show.new(id: id, resource: resource, params: params) }
+
+        it "sets id" do
+          expect(show.id).to eq(id)
+        end
+
+        it "sets resource" do
+          expect(show.resource).to eq(resource)
+        end
+
+        it "sets params" do
+          expect(show.params).to eq(params)
+        end
+      end
+    end
+  end
+end
