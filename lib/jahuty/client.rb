893561ab11c1942376249a6b013d40054d48d569
Jack Clayton <clayjs0@gmail.com>
2020-12-29 16:20:20 -0700

    Add jahuty client

---
+++ lib/jahuty/client.rb
@@ -0,0 +1,43 @@
+module Jahuty
+  class Client
+    @api_key
+
+    @client
+
+    @requests
+
+    @resources
+
+    @services
+
+    def initialize (api_key:)
+      @api_key = api_key
+    end
+
+    def method_missing(name, *arguments)
+      if arguments.empty?
+        @services = Service::Factory.new(client: self) if @services.nil?
+
+        @services.send(name.to_sym)
+      end
+    end
+
+    def request(action)
+      @requests = Request::Factory.new if @requests.nil?
+
+      request = @requests.call(action)
+
+      @client =  Api::Client.new(api_key: @api_key) if @client.nil?
+
+      response = @client.send(request)
+
+      @resources = Resource::Factory.new if @resources.nil?
+
+      resource = @resources.call(action, response)
+
+      raise Exception::Error.new(resource) if resource.is_a?(Resource::Problem)
+
+      resource
+    end
+  end
+end
---
+++ spec/jahuty/client_spec.rb
@@ -0,0 +1,39 @@
+module Jahuty
+  RSpec.describe Client do
+    let(:action) { Action::Show.new(resource: 'render', id: 1) }
+    let(:url)    { "https://api.jahuty.com/snippets/1/render" }
+
+    subject(:client) { Client.new(api_key: "foo") }
+
+    describe "#request" do
+      context "when the action succeeds" do
+        before do
+          stub_request(:get, url).
+            to_return(status: 200, body: '{"content":"foo"}', headers: {})
+        end
+
+        it "returns render" do
+          expect(client.request(action)).to be_instance_of(Resource::Render)
+        end
+      end
+
+      context "when the action fails" do
+        before do
+          stub_request(:get, url).
+            to_return(
+              status:  404,
+              body:    '{ "status":404, "type": "foo", "detail": "bar" }',
+              headers: {
+                'Content-Type' => 'application/problem+json'
+              })
+        end
+
+        it "raises error" do
+          expect {
+            client.request(action)
+          }.to raise_error(Exception::Error)
+        end
+      end
+    end
+  end
+end
