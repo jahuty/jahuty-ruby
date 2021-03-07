b4dff64bd3054e4e16c776a05ac390dfd49fcdd7
Jack Clayton <clayjs0@gmail.com>
2021-03-06 18:31:38 -0700

    Move response handling out of resource factory

--- lib/jahuty/resource/factory.rb
+++ lib/jahuty/resource/factory.rb
@@ -1,49 +1,26 @@
 # frozen_string_literal: true
 
-require 'json'
-
 module Jahuty
   module Resource
-    # Negotiates the resource to return given the requested action and the
-    # server's response.
+    # Instantiates and returns a resource.
     class Factory
       CLASSES = {
         problem: Problem.name,
         render: Render.name
       }.freeze
 
-      def call(action, response)
-        if success? response
-          resource_name = action.resource
-        elsif problem? response
-          resource_name = 'problem'
-        else
-          raise ArgumentError, 'Unexpected response'
-        end
+      def call(resource_name, payload)
+        klass = class_name(resource_name.to_sym)
 
-        resource_class = class_name(resource_name.to_sym)
+        raise ArgumentError, "#{resource_name} missing" if klass.nil?
 
-        payload = parse(response)
-
-        Object.const_get(resource_class).send(:from, **payload)
+        Object.const_get(klass).send(:from, **payload)
       end
 
       private
 
       def class_name(resource_name)
         CLASSES[resource_name.to_sym]
-      end
-
-      def problem?(response)
-        response.headers['Content-Type'] == 'application/problem+json'
-      end
-
-      def parse(response)
-        JSON.parse(response.body, symbolize_names: true)
-      end
-
-      def success?(response)
-        response.status.between?(200, 299)
       end
     end
   end
--- spec/jahuty/resource/factory_spec.rb
+++ spec/jahuty/resource/factory_spec.rb
@@ -6,85 +6,15 @@ module Jahuty
       describe '#call' do
         subject(:factory) { described_class.new }
 
-        # A valid action.
-        let(:action) { ::Jahuty::Action::Show.new(id: 1, resource: 'render') }
-
-        context 'with the requested resource' do
-          let(:response) do
-            response = instance_double('::Faraday::Response')
-
-            allow(response).to receive(:status).and_return(200)
-            allow(response).to receive(:body).and_return('{"content":"foo"}')
-
-            response
-          end
-
-          it 'returns render' do
-            expect(factory.call(action, response)).to be_instance_of(Render)
-          end
-        end
-
-        context 'with invalid json' do
-          let(:response) do
-            response = instance_double('::Faraday::Response')
-
-            allow(response).to receive(:status).and_return(200)
-            allow(response).to receive(:body).and_return('invalid json')
-
-            response
-          end
-
-          it 'raises error' do
-            expect { factory.call(action, response) }.to raise_error(JSON::ParserError)
-          end
-        end
-
-        context 'with a problem resource' do
-          let(:response) do
-            headers = instance_double('::Faraday::Headers')
-
-            # The Content-Type header indicates a problem resource.
-            allow(headers).to receive(:[])
-              .with('Content-Type')
-              .and_return('application/problem+json')
-
-            response = instance_double('::Faraday::Response')
-
-            allow(response).to receive(:status).and_return(404)
-            allow(response).to receive(:headers).and_return(headers)
-            allow(response).to receive(:body).and_return(
-              '{"status": 200, "type": "foo", "detail": "bar" }'
-            )
-
-            response
-          end
-
-          it 'returns problem' do
-            expect(factory.call(action, response)).to be_instance_of(Problem)
-          end
-        end
-
         context 'with an invalid resource' do
-          let(:response) do
-            headers = instance_double('::Faraday::Headers')
-
-            # Use any Content-Type, except application/problem+json.
-            allow(headers).to receive(:[])
-              .with('Content-Type')
-              .and_return('text/plain')
-
-            response = instance_double('::Faraday::Response')
-
-            # Use any status code outside of 2xx range.
-            allow(response).to receive(:status).and_return(404)
-            allow(response).to receive(:headers).and_return(headers)
-            allow(response).to receive(:body).and_return('a fatal error')
-
-            response
-          end
-
           it 'raises error' do
-            expect { factory.call(action, response) }.to raise_error(ArgumentError)
+            expect { factory.call 'foo', {} }.to raise_error(ArgumentError)
+          end
+        end
+
+        context 'with a valid resource' do
+          it 'returns render' do
+            expect(factory.call 'render', { content: 'foo' }).to be_instance_of(Render)
           end
         end
       end
